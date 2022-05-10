1d4009c471a56a1d6cf07a0c6ac1382e68bfe83a
nonz250 <hosaka.non.work@gmail.com>
2022-05-10 23:10:49 +0900

    Draft: add client template.

---
+++ tests/Client/ClientTest.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+namespace Client;
+
+use PHPUnit\Framework\TestCase;
+use StoragePhpClient\Client\Client;
+use StoragePhpClient\Client\ClientInterface;
+
+class ClientTest extends TestCase
+{
+    public function test__construct(): ClientInterface
+    {
+        $client = new Client();
+        $this->assertInstanceOf(ClientInterface::class, $client);
+        return $client;
+    }
+}
