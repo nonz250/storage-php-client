e20c0ed82d01a6e0fdf2c586c75613332f4ec50f
nonz250 <hosaka.non.work@gmail.com>
2024-07-06 06:17:58 +0900

    test: api param test のカバレッジを修正

--- tests/Foundation/ApiParamTest.php
+++ tests/Foundation/ApiParamTest.php
@@ -8,7 +8,7 @@ use PHPUnit\Framework\TestCase;
 
 final class ApiParamTest extends TestCase
 {
-    public function test__construct()
+    public function testNoBody()
     {
         $apiParam = new class extends ApiParam {
             public function getMethod(): string
@@ -24,5 +24,37 @@ final class ApiParamTest extends TestCase
         $this->assertInstanceOf(ApiParam::class, $apiParam);
         $this->assertSame('GET', $apiParam->getMethod());
         $this->assertSame('/path', $apiParam->getPath());
+        $this->assertSame([], $apiParam->getHeaders());
+        $this->assertSame([], $apiParam->getQueryParams());
+        $this->assertNull($apiParam->getBody());
+    }
+
+    public function testJsonBody()
+    {
+        $jsonApiParam = new class() extends ApiParam implements \JsonSerializable {
+            public function getMethod(): string
+            {
+                return 'POST';
+            }
+
+            public function getPath(): string
+            {
+                return '/post';
+            }
+
+            /**
+             * @return string[]
+             */
+            public function jsonSerialize(): array
+            {
+                return ['key' => 'value'];
+            }
+        };
+        $this->assertInstanceOf(ApiParam::class, $jsonApiParam);
+        $this->assertSame('POST', $jsonApiParam->getMethod());
+        $this->assertSame('/post', $jsonApiParam->getPath());
+        $this->assertSame([], $jsonApiParam->getHeaders());
+        $this->assertSame([], $jsonApiParam->getQueryParams());
+        $this->assertJson(json_encode($jsonApiParam->getBody()));
     }
 }
