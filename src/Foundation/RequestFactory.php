8c5f227365fe7d294bc739649f90e08ab8d2698f
nonz250 <hosaka.non.work@gmail.com>
2024-07-06 06:56:17 +0900

    feat: request factory を追加

---
+++ src/Foundation/RequestFactory.php
@@ -0,0 +1,108 @@
+<?php
+declare(strict_types=1);
+
+namespace StoragePhpClient\Foundation;
+
+use JsonException;
+use JsonSerializable;
+use Psr\Http\Message\RequestFactoryInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\StreamFactoryInterface;
+use RuntimeException;
+
+final class RequestFactory
+{
+    public function __construct(
+        private readonly RequestFactoryInterface $requestFactory,
+        private readonly StreamFactoryInterface $streamFactory,
+    ) {
+    }
+
+    /**
+     * @param string $endpoint
+     * @param string $path
+     * @param array<string, mixed> $queryParams
+     * @return string
+     */
+    public static function buildUriString(string $endpoint, string $path, array $queryParams): string
+    {
+        if ($path !== '' && mb_strpos($path, '/') !== 0) {
+            $path = '/' . $path;
+        }
+
+        $uri = $endpoint . $path;
+
+        $queryString = self::buildQueryString($queryParams);
+        if ($queryString !== '') {
+            $uri .= '?' . $queryString;
+        }
+
+        return $uri;
+    }
+
+    /**
+     * @param array $queryParams
+     * @return string
+     */
+    public static function buildQueryString(array $queryParams): string
+    {
+        if ($queryParams === []) {
+            return '';
+        }
+
+        return http_build_query($queryParams, '', '&', PHP_QUERY_RFC3986);
+    }
+
+    /**
+     * @param Credential $credential
+     * @param ApiParam $apiParam
+     * @return RequestInterface
+     */
+    public function create(Credential $credential, ApiParam $apiParam): RequestInterface
+    {
+        $request = $this->requestFactory->createRequest(
+            $apiParam->getMethod(),
+            self::buildUriString($credential->getEndpoint(), $apiParam->getPath(), $apiParam->getQueryParams()),
+        );
+
+        foreach ($apiParam->getHeaders() as $key => $value) {
+            $request = $request->withHeader($key, $value);
+        }
+
+        $request = $request->withHeader('Authorization', $credential->authorizationDigest(
+            $apiParam->getMethod(),
+            $apiParam->getPath(),
+        ));
+
+        $body = $apiParam->getBody();
+        if ($body instanceof JsonSerializable) {
+            // 現状は application/json のみ
+            try {
+                $json = json_encode(
+                    $body,
+                    JSON_UNESCAPED_SLASHES
+                    | JSON_UNESCAPED_UNICODE
+                    | JSON_PRESERVE_ZERO_FRACTION
+                    | JSON_PARTIAL_OUTPUT_ON_ERROR
+                    | JSON_THROW_ON_ERROR
+                );
+            } catch (JsonException $e) { // @codeCoverageIgnore
+                throw new RuntimeException('Failed to encode json.', $e->getCode(), $e); // @codeCoverageIgnore
+            }
+
+            if ($json === 'null') {
+                // json 文字列化して "null" になるということは、リクエストボディなしを指す
+                return $request; // @codeCoverageIgnore
+            }
+
+            $stream = $this->streamFactory->createStream($json);
+            unset($json);
+
+            $request = $request
+                ->withHeader('Content-Type', ContentType::JSON->value)
+                ->withBody($stream);
+        }
+
+        return $request;
+    }
+}
---
+++ tests/Foundation/RequestFactoryTest.php
@@ -0,0 +1,274 @@
+<?php
+declare(strict_types=1);
+
+namespace Foundation;
+
+use Fig\Http\Message\RequestMethodInterface;
+use Http\Discovery\Psr17FactoryDiscovery;
+use JsonSerializable;
+use PHPUnit\Framework\MockObject\MockObject;
+use Psr\Http\Message\StreamFactoryInterface;
+use StoragePhpClient\Foundation\ApiParam;
+use StoragePhpClient\Foundation\Credential;
+use StoragePhpClient\Foundation\RequestFactory;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @see RequestFactory
+ * @coversDefaultClass \StoragePhpClient\Foundation\RequestFactory
+ */
+final class RequestFactoryTest extends TestCase
+{
+    private const PATH = '/foo';
+
+    private static Credential $credential;
+
+    public static function setUpBeforeClass(): void
+    {
+        self::$credential = new Credential('https://foo:bar@example.com', 'nonce');
+    }
+
+
+    public function test__construct()
+    {
+        $requestFactory = new RequestFactory(
+            Psr17FactoryDiscovery::findRequestFactory(),
+            $this->createStub(StreamFactoryInterface::class)
+        );
+        $this->assertInstanceOf(RequestFactory::class, $requestFactory);
+    }
+
+    /**
+     * @return void
+     * @covers ::buildUriString
+     */
+    public function testBuildUrlString()
+    {
+        $this->assertSame(
+            'https://example.com/api/v1/test',
+            RequestFactory::buildUriString('https://example.com', 'api/v1/test', [])
+        );
+        $this->assertSame(
+            'https://example.com/api/v1/test',
+            RequestFactory::buildUriString('https://example.com', '/api/v1/test', [])
+        );
+        $this->assertSame(
+            'https://example.com/api/v1/test?foo=bar',
+            RequestFactory::buildUriString('https://example.com', 'api/v1/test', ['foo' => 'bar'])
+        );
+        $this->assertSame(
+            'https://example.com/api/v1/test?foo=bar&baz=qux',
+            RequestFactory::buildUriString('https://example.com', 'api/v1/test', ['foo' => 'bar', 'baz' => 'qux'])
+        );
+    }
+
+    /**
+     * @test GETリクエストの作成を検証する
+     */
+    public function createGetRequest(): void
+    {
+        $requestFactory = new RequestFactory(
+            Psr17FactoryDiscovery::findRequestFactory(),
+            $this->createStub(StreamFactoryInterface::class)
+        );
+
+        $apiParam = $this->createApiParamMock(RequestMethodInterface::METHOD_GET, self::PATH);
+
+        $request = $requestFactory->create(self::$credential, $apiParam);
+
+        $this->assertSame(RequestMethodInterface::METHOD_GET, $request->getMethod());
+        $this->assertStringStartsWith(
+            RequestFactory::buildUriString(self::$credential->getEndpoint(), self::PATH, []),
+            (string)$request->getUri()
+        );
+        $this->assertSame('', $request->getHeaderLine('Content-Type'));
+    }
+
+    /**
+     * @test リクエストヘッダーを含むGETリクエストの作成を検証する
+     */
+    public function createGetRequestWithHeader(): void
+    {
+        $requestFactory = new RequestFactory(
+            Psr17FactoryDiscovery::findRequestFactory(),
+            $this->createStub(StreamFactoryInterface::class)
+        );
+
+        $header = ['key' => 'value'];
+        $apiParam = $this->createApiParamMock(RequestMethodInterface::METHOD_GET, self::PATH);
+        $apiParam->method('getHeaders')->willReturn($header);
+
+        $request = $requestFactory->create(self::$credential, $apiParam);
+
+        $this->assertSame(RequestMethodInterface::METHOD_GET, $request->getMethod());
+        $this->assertStringStartsWith(
+            RequestFactory::buildUriString(self::$credential->getEndpoint(), self::PATH, []),
+            (string)$request->getUri()
+        );
+        $this->assertSame('', $request->getHeaderLine('Content-Type'));
+        $this->assertSame($header['key'], $request->getHeaderLine('key'));
+    }
+
+    /**
+     * @test クエリパラメーターを含むGETリクエストの作成を検証する
+     */
+    public function createGetRequestWithQuery(): void
+    {
+        $requestFactory = new RequestFactory(
+            Psr17FactoryDiscovery::findRequestFactory(),
+            $this->createStub(StreamFactoryInterface::class)
+        );
+
+        $query = ['key' => 'value'];
+        $apiParam = $this->createApiParamMock(RequestMethodInterface::METHOD_GET, self::PATH);
+        $apiParam->method('getQueryParams')->willReturn($query);
+
+        $request = $requestFactory->create(self::$credential, $apiParam);
+
+        $this->assertSame(RequestMethodInterface::METHOD_GET, $request->getMethod());
+        $this->assertStringStartsWith(
+            RequestFactory::buildUriString(self::$credential->getEndpoint(), self::PATH, $query),
+            (string)$request->getUri()
+        );
+        $this->assertSame(RequestFactory::buildQueryString($query), $request->getUri()->getQuery());
+        $this->assertSame('', $request->getHeaderLine('Content-Type'));
+    }
+
+    /**
+     * @test POSTリクエストの作成を検証する
+     */
+    public function createPostRequest(): void
+    {
+        $requestFactory = new RequestFactory(
+            Psr17FactoryDiscovery::findRequestFactory(),
+            $this->createStub(StreamFactoryInterface::class)
+        );
+
+        $apiParam = $this->createApiParamMock(RequestMethodInterface::METHOD_POST, self::PATH);
+
+        $request = $requestFactory->create(self::$credential, $apiParam);
+
+        $this->assertSame(RequestMethodInterface::METHOD_POST, $request->getMethod());
+        $this->assertStringStartsWith(
+            RequestFactory::buildUriString(self::$credential->getEndpoint(), self::PATH, []),
+            (string)$request->getUri()
+        );
+        $this->assertSame('', $request->getHeaderLine('Content-Type'));
+    }
+
+    /**
+     * @test リクエストボディを含むPOSTリクエストの作成を検証する
+     */
+    public function createPostRequestWithBody(): void
+    {
+        $requestFactory = new RequestFactory(
+            Psr17FactoryDiscovery::findRequestFactory(),
+            Psr17FactoryDiscovery::findStreamFactory()
+        );
+
+        $apiParam = $this->createApiParamMock(RequestMethodInterface::METHOD_POST, self::PATH);
+        $apiParam->method('getBody')->willReturn(
+            new class() implements JsonSerializable {
+                /**
+                 * @return array<string, string>
+                 */
+                public function jsonSerialize(): array
+                {
+                    return ['key' => 'value'];
+                }
+            }
+        );
+
+        $request = $requestFactory->create(self::$credential, $apiParam);
+
+        $this->assertSame(RequestMethodInterface::METHOD_POST, $request->getMethod());
+        $this->assertStringStartsWith(
+            RequestFactory::buildUriString(self::$credential->getEndpoint(), self::PATH, []),
+            (string)$request->getUri()
+        );
+        $this->assertSame('application/json', $request->getHeaderLine('Content-Type'));
+        $this->assertJson($request->getBody()->getContents());
+    }
+
+    /**
+     * @test PUTリクエストの作成を検証する
+     */
+    public function createPutRequest(): void
+    {
+        $requestFactory = new RequestFactory(
+            Psr17FactoryDiscovery::findRequestFactory(),
+            $this->createStub(StreamFactoryInterface::class)
+        );
+
+        $apiParam = $this->createApiParamMock(RequestMethodInterface::METHOD_PUT, self::PATH);
+
+        $request = $requestFactory->create(self::$credential, $apiParam);
+
+        $this->assertSame(RequestMethodInterface::METHOD_PUT, $request->getMethod());
+        $this->assertStringStartsWith(
+            RequestFactory::buildUriString(self::$credential->getEndpoint(), self::PATH, []),
+            (string)$request->getUri()
+        );
+        $this->assertSame('', $request->getHeaderLine('Content-Type'));
+    }
+
+    /**
+     * @test PATCHリクエストの作成を検証する
+     */
+    public function createPatchRequest(): void
+    {
+        $requestFactory = new RequestFactory(
+            Psr17FactoryDiscovery::findRequestFactory(),
+            $this->createStub(StreamFactoryInterface::class)
+        );
+
+        $apiParam = $this->createApiParamMock(RequestMethodInterface::METHOD_PATCH, self::PATH);
+
+        $request = $requestFactory->create(self::$credential, $apiParam);
+
+        $this->assertSame(RequestMethodInterface::METHOD_PATCH, $request->getMethod());
+        $this->assertStringStartsWith(
+            RequestFactory::buildUriString(self::$credential->getEndpoint(), self::PATH, []),
+            (string)$request->getUri()
+        );
+        $this->assertSame('', $request->getHeaderLine('Content-Type'));
+    }
+
+    /**
+     * @test DELETEリクエストの作成を検証する
+     */
+    public function createDeleteRequest(): void
+    {
+        $requestFactory = new RequestFactory(
+            Psr17FactoryDiscovery::findRequestFactory(),
+            $this->createStub(StreamFactoryInterface::class)
+        );
+
+        $apiParam = $this->createApiParamMock(RequestMethodInterface::METHOD_DELETE, self::PATH);
+
+        $request = $requestFactory->create(self::$credential, $apiParam);
+
+        $this->assertSame(RequestMethodInterface::METHOD_DELETE, $request->getMethod());
+        $this->assertStringStartsWith(
+            RequestFactory::buildUriString(self::$credential->getEndpoint(), self::PATH, []),
+            (string)$request->getUri()
+        );
+        $this->assertSame('', $request->getHeaderLine('Content-Type'));
+    }
+
+
+    /**
+     * {@link ApiParam のモックを作成する}
+     * @param string $method
+     * @param string $path
+     * @return ApiParam&MockObject
+     */
+    private function createApiParamMock(string $method, string $path): ApiParam&MockObject
+    {
+        $apiParam = $this->createMock(ApiParam::class);
+        $apiParam->method('getMethod')->willReturn($method);
+        $apiParam->method('getPath')->willReturn($path);
+
+        return $apiParam;
+    }
+}
